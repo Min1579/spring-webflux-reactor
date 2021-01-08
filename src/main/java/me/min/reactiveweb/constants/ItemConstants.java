3f599912c9d1b2b0c0c034d0ff5c9d3fd50067be
Min1579 <hsm012362@gmail.com>
2021-01-08 17:17:05 +0900

    feat: ⚡ define api endpoint as constants

--- src/main/java/me/min/reactiveweb/constants/ItemConstants.java
+++ src/main/java/me/min/reactiveweb/constants/ItemConstants.java
@@ -3,7 +3,13 @@ package me.min.reactiveweb.constants;
 public class ItemConstants {
     public static final String ITEMS_END_POINT_V1 = "/v1/items";
     public static final String ITEM_BY_ID_END_POINT_V1 = "/v1/item/";
-    public static final String ITEM_REGISTER_END_POINT_V1 = "/v1/item/";
+    public static final String ITEM_REGISTER_END_POINT_V1 = "/v1/item";
     public static final String ITEM_UPDATE_END_POINT_V1 = "/v1/item/";
     public static final String ITEM_DELETE_END_POINT_V1 = "/v1/item/";
+
+    public static final String ITEMS_END_FUN_POINT_V1 = "/v1/fun/items";
+    public static final String ITEM_BY_ID_FUN_END_POINT_V1 = "/v1/fun/item/";
+    public static final String ITEM_REGISTER_FUN_END_POINT_V1 = "/v1/fun/item";
+    public static final String ITEM_UPDATE_FUN_END_POINT_V1 = "/v1/fun/item/";
+    public static final String ITEM_DELETE_FUN_END_POINT_V1 = "/v1/fun/item/";
 }
