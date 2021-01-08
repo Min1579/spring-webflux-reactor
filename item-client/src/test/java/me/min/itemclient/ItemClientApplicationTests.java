4816e04013b33e3f1aade482a18a81f69c3c1ca2
Min1579 <hsm012362@gmail.com>
2021-01-08 17:50:04 +0900

    feat ⚡️ new project init

---
+++ item-client/src/test/java/me/min/itemclient/ItemClientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.min.itemclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
