624d132d293d818b4e9f10f8502a277ccb42c8c1
Min1579 <hsm012362@gmail.com>
2021-01-09 21:26:34 +0900

    feat: added controllerAdvice

---
+++ reactive-spring/src/main/java/me/min/reactiveweb/exception/ControllerExceptionHandler.java
@@ -0,0 +1,20 @@
+package me.min.reactiveweb.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        log.error("Exception caught in handleRuntimeException : {]", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Runtime Exception from Controller Advice");
+    }
+}
